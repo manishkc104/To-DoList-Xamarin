08c7a36700eb5a8a4286f8761d9714c33ed89c90
Manish KC <manishkc@sportsconnect.io>
2019-04-08 13:58:49 +0545

    My first commit

---
+++ TodoList/TodoList/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TodoList.Data;
+using TodoList.Views;
+using Xamarin.Forms;
+
+namespace TodoList
+{
+	public partial class App : Application
+	{
+        static TodoItemDatabase database;
+		public App ()
+		{
+			InitializeComponent();
+            Resources = new ResourceDictionary();
+            Resources.Add("primaryGreen", Color.FromHex("91CA47"));
+            Resources.Add("primaryDarkGreen", Color.FromHex("6FA22E"));
+            var nav = new NavigationPage(new TodoListPage());
+            nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+            nav.BarTextColor = Color.White;
+        
+            MainPage = nav;
+           
+		}
+        public static TodoItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new TodoItemDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TodoSQLite.db3"));
+                }
+                return database;
+            }
+        }
+        public int ResumeAtTodoId { get; set; }
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ TodoList/TodoList/Data/TodoItemDatabase.cs
@@ -0,0 +1,76 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class TodoItemDatabase
+    {
+        readonly  SQLiteAsyncConnection database;
+        public TodoItemDatabase(string _dbPath)
+        {
+            database = new SQLiteAsyncConnection(_dbPath);
+            database.CreateTableAsync <TodoItem>().Wait();
+        }
+        public Task<List<TodoItem>> GetItemsAsync()
+
+        {
+
+            return database.Table<TodoItem>().ToListAsync();
+
+        }
+        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+
+        {
+
+            return database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+
+        }
+
+
+
+        public Task<TodoItem> GetItemAsync(int id)
+
+        {
+
+            return database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+
+        }
+
+
+
+        public Task<int> SaveItemAsync(TodoItem item)
+
+        {
+
+            if (item.ID != 0)
+
+            {
+
+                return database.UpdateAsync(item);
+
+            }
+
+            else
+            {
+
+                return database.InsertAsync(item);
+
+            }
+
+        }
+
+
+
+        public Task<int> DeleteItemAsync(TodoItem item)
+
+        {
+
+            return database.DeleteAsync(item);
+
+        }
+    }
+}
